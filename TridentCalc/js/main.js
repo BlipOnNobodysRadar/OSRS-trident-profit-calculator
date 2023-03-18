3fa09d0fc7b25c913e744386acaf3a17d13c9408
unknown <ryanlowe.codeblip@gmail.com>
2023-03-17 20:15:05 -0400

    JS refactor

--- TridentCalc/js/main.js
+++ TridentCalc/js/main.js
@@ -1,146 +1,158 @@
-let button = document.querySelector("#button");
-let inputs = document.querySelectorAll("input");
-let profitThreshold = document.querySelector(".profit-threshold");
-let audioNotification = document.querySelector("#audio-notification");
-
-class Calculator {
-  constructor() {
-    this.number = document.querySelector("#number");
-    this.deaths = document.querySelector("#deathCost");
-    this.chaos = document.querySelector("#chaosCost");
-    this.fires = document.querySelector("#fireCost");
-    this.tridents = document.querySelector("#tridentCost");
-    this.sellValue = document.querySelector("#sellPrice");
-    this.deathsCost = document.querySelector(".scaledDeaths");
-    this.chaosCost = document.querySelector(".scaledChaos");
-    this.fireCost = document.querySelector(".scaledFires");
-    this.tridentsCost = document.querySelector(".scaledTridents");
-    this.castsCost = document.querySelector(".scaledCasts");
-    this.scaledSales = document.querySelector(".scaledSales");
-    this.combinedCostElement = document.querySelector(".combinedCost");
-    this.taxCost = document.querySelector(".taxCost");
-    this.totalProfit = document.querySelector(".totalProfit");
-    this.tax = 0.01;
-    this.totalTax = 0;
-    this.combinedCost = 0;
-    this.saleValue;
-  }
-  async setValuesFromGE() {
-    let itemInfo = {};
-    await fetch("https://prices.runescape.wiki/api/v1/osrs/latest")
-      .then((res) => res.json())
-      .then((data) => {
-        itemInfo.deathRune = data.data[560];
-        itemInfo.chaosRune = data.data[562];
-        itemInfo.fireRune = data.data[554];
-        itemInfo.unchargedTrident = data.data[11908];
-        itemInfo.chargedTrident = data.data[11905];
-
-        this.deaths.value = itemInfo.deathRune.low;
-        this.fires.value = itemInfo.fireRune.low;
-        this.chaos.value = itemInfo.chaosRune.low;
-        this.tridents.value = itemInfo.unchargedTrident.low;
-        this.sellValue.value = itemInfo.chargedTrident.high;
-
-        this.renderAll();
-      });
-  }
-  renderTridentCost() {
-    const cost = this.number.value * this.tridents.value;
-    this.combinedCost = this.combinedCost + cost;
-    this.tridentsCost.innerHTML = `${cost.toLocaleString()} (${this.number.value.toLocaleString()})`;
-  }
-  renderDeathsCost() {
-    const cost = this.number.value * this.deaths.value * 2500;
-    this.combinedCost = this.combinedCost + cost;
-    this.deathsCost.innerHTML = `${cost.toLocaleString()} (${(
-      2500 * this.number.value
-    ).toLocaleString()})`;
-  }
-  renderChaosCost() {
-    const cost = this.number.value * this.chaos.value * 2500;
-    this.combinedCost = this.combinedCost + cost;
-    this.chaosCost.innerHTML = `${cost.toLocaleString()} (${(
-      2500 * this.number.value
-    ).toLocaleString()})`;
-  }
-  renderFiresCost() {
-    const cost = this.number.value * this.fires.value * 12500;
-    this.combinedCost = this.combinedCost + cost;
-    this.fireCost.innerHTML = `${cost.toLocaleString()} (${(
-      12500 * this.number.value
-    ).toLocaleString()})`;
-  }
-  renderCastsCost() {
-    const CAST_COST = 10;
-    const cost = this.number.value * CAST_COST * 2500;
-    this.combinedCost = this.combinedCost + cost;
-    this.castsCost.innerHTML = `${cost.toLocaleString()} (${(
-      2500 * this.number.value
-    ).toLocaleString()})`;
-  }
-  renderSaleValue() {
-    this.saleValue = this.sellValue.value * this.number.value;
-    this.scaledSales.innerHTML = this.saleValue.toLocaleString();
-  }
-  renderTax() {
-    this.totalTax = this.tax * this.saleValue;
-    this.taxCost.innerHTML = this.totalTax.toLocaleString();
-  }
-  renderCombinedCost() {
-    this.combinedCostElement.innerHTML = this.combinedCost.toLocaleString();
-  }
-  renderProfit() {
-    this.totalProfit.innerHTML = Math.round(
-      this.saleValue - this.combinedCost - this.tax
-    ).toLocaleString();
-  }
-
-  renderAll() {
-    this.combinedCost = 0;
-    this.renderTridentCost();
-    this.renderChaosCost();
-    this.renderDeathsCost();
-    this.renderFiresCost();
-    this.renderCastsCost();
-    this.renderSaleValue();
-    this.renderTax();
-    this.renderCombinedCost();
-    this.renderProfit();
-    if (document.querySelector("#notify").checked) {
-      const singleProfit =
-        Math.round(this.saleValue - this.combinedCost - this.tax) /
-        this.number.value;
-      if (singleProfit >= profitThreshold.value) {
-        audioNotification.play();
-      }
+class API {
+  // Fetches the latest Grand Exchange data for required items
+  static async fetchGEData() {
+    try {
+      const response = await fetch("https://prices.runescape.wiki/api/v1/osrs/latest");
+      const data = await response.json();
+      return {
+        deathRuneLow: data.data[560].low,
+        chaosRuneLow: data.data[562].low,
+        fireRuneLow: data.data[554].low,
+        unchargedTridentLow: data.data[11908].low,
+        chargedTridentHigh: data.data[11905].high,
+      };
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      return null;
     }
   }
 }
-let calculator = new Calculator();
-calculator.setValuesFromGE();
-calculator.renderAll();
-inputs.forEach((input) =>
-  input.addEventListener("input", () => {
-    calculator = new Calculator();
-    calculator.renderAll();
-  })
-);
-button.addEventListener("click", resetAndRender());
-let autoupdateInterval;
-document.querySelector("#autoupdate").addEventListener("change", () => {
-  if (document.querySelector("#autoupdate").checked) {
-    autoupdateInterval = setInterval(() => {
-      calculator.setValuesFromGE();
-      calculator.renderAll();
-    }, 120000);
-  } else {
-    clearInterval(autoupdateInterval);
-  }
-});
 
-function resetAndRender() {
-  document.querySelector("form").reset();
-  calculator = new Calculator();
-  calculator.renderAll();
+class Calculator {
+  constructor(itemInfo = {}) {
+    this.itemInfo = itemInfo;
+    this.tax = 0.01;
+    // Initialize DOM elements
+    this.elements = {
+      number: document.querySelector("#number"),
+      deaths: document.querySelector("#deathCost"),
+      chaos: document.querySelector("#chaosCost"),
+      fires: document.querySelector("#fireCost"),
+      tridents: document.querySelector("#tridentCost"),
+      sellValue: document.querySelector("#sellPrice"),
+      deathsCost: document.querySelector(".scaledDeaths"),
+      chaosCost: document.querySelector(".scaledChaos"),
+      fireCost: document.querySelector(".scaledFires"),
+      tridentsCost: document.querySelector(".scaledTridents"),
+      castsCost: document.querySelector(".scaledCasts"),
+      scaledSales: document.querySelector(".scaledSales"),
+      combinedCostElement: document.querySelector(".combinedCost"),
+      taxCost: document.querySelector(".taxCost"),
+      totalProfit: document.querySelector(".totalProfit"),
+    };
+
+    this.updateAndRender(true);
+  }
+
+  // Updates values and renders the results
+  updateAndRender(updateInputs = false) {
+    if (updateInputs) {
+      this.elements.deaths.value = this.itemInfo.deathRuneLow;
+      this.elements.chaos.value = this.itemInfo.chaosRuneLow;
+      this.elements.fires.value = this.itemInfo.fireRuneLow;
+      this.elements.tridents.value = this.itemInfo.unchargedTridentLow;
+      this.elements.sellValue.value = this.itemInfo.chargedTridentHigh;
+    }
+    this.renderAll();
+  }
+
+  // Calculates the costs, sales, and profits
+  calculateCosts() {
+    const number = parseFloat(this.elements.number.value);
+    const deathRuneCost = parseFloat(this.elements.deaths.value);
+    const fireRuneCost = parseFloat(this.elements.fires.value);
+    const chaosRuneCost = parseFloat(this.elements.chaos.value);
+    const unchargedTridentCost = parseFloat(this.elements.tridents.value);
+    const chargedTridentSellPrice = parseFloat(this.elements.sellValue.value);
+
+    return {
+      tridentCost: number * unchargedTridentCost,
+      chaosCost: number * chaosRuneCost * 2500,
+      deathsCost: number * deathRuneCost * 2500,
+      firesCost: number * fireRuneCost * 12500,
+      castsCost: number * 10 * 2500,
+      saleValue: number * chargedTridentSellPrice,
+    };
+  }
+
+  // Renders the calculated costs, sales, and profits in the DOM
+  renderAll() {
+    const costs = this.calculateCosts();
+    const combinedCost = costs.tridentCost + costs.chaosCost + costs.deathsCost + costs.firesCost + costs.castsCost;
+    const taxCost = costs.saleValue * this.tax;
+
+    this.elements.tridentsCost.textContent =     `${costs.tridentCost.toLocaleString()} (${this.elements.number.value.toLocaleString()})`;
+    this.elements.chaosCost.textContent = `${costs.chaosCost.toLocaleString()} (${(2500 * this.elements.number.value).toLocaleString()})`;
+    this.elements.deathsCost.textContent = `${costs.deathsCost.toLocaleString()} (${(2500 * this.elements.number.value).toLocaleString()})`;
+    this.elements.fireCost.textContent = `${costs.firesCost.toLocaleString()} (${(12500 * this.elements.number.value).toLocaleString()})`;
+    this.elements.castsCost.textContent = `${costs.castsCost.toLocaleString()} (${(2500 * this.elements.number.value).toLocaleString()})`;
+    this.elements.scaledSales.textContent = costs.saleValue.toLocaleString();
+    this.elements.combinedCostElement.textContent = combinedCost.toLocaleString();
+    this.elements.taxCost.textContent = taxCost.toLocaleString();
+    this.elements.totalProfit.textContent = (costs.saleValue - combinedCost - taxCost).toLocaleString();
+
+    // Notify profit
+    const profitThreshold = parseFloat(document.querySelector(".profit-threshold").value);
+    const singleProfit = (costs.saleValue - combinedCost - taxCost) / parseFloat(this.elements.number.value);
+    if (document.querySelector("#notify").checked && singleProfit >= profitThreshold) {
+      document.querySelector("#audio-notification").play();
+    }
+  }
 }
+
+async function initializeCalculator() {
+  const itemInfo = await API.fetchGEData();
+  if (!itemInfo) {
+    // Handle API error and display a message to the user.
+    return;
+  }
+  const calculator = new Calculator(itemInfo);
+
+  // Set up event listeners for inputs and buttons.
+  const inputs = document.querySelectorAll("input");
+  const button = document.querySelector("#button");
+
+  inputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      calculator.updateAndRender(false);
+    });
+  });
+
+  button.addEventListener("click", () => {
+    document.querySelector("form").reset();
+    calculator.updateAndRender(true);
+  });
+
+  // Add event listener for the "autoupdate" checkbox.
+  const autoupdateCheckbox = document.querySelector("#autoupdate");
+  let autoupdateInterval;
+  autoupdateCheckbox.addEventListener("change", async () => {
+    if (autoupdateCheckbox.checked) {
+      const updatedItemInfo = await API.fetchGEData();
+      if (updatedItemInfo) {
+        calculator.itemInfo = updatedItemInfo;
+        calculator.updateAndRender(true);
+      }
+      autoupdateInterval = setInterval(async () => {
+        const updatedItemInfo = await API.fetchGEData();
+        if (updatedItemInfo) {
+          calculator.itemInfo = updatedItemInfo;
+          calculator.updateAndRender();
+        }
+      }, 120000);
+    } else {
+      clearInterval(autoupdateInterval);
+    }
+  });
+
+  return calculator;
+}
+
+initializeCalculator()
+  .then((calculator) => {
+    // Event listeners are already set up in the initializeCalculator function, so no need to add them again here.
+  })
+  .catch((error) => {
+    console.error("Error initializing calculator:", error);
+  });
+
---
+++ TridentCalc/js/pre-refactor.js
@@ -0,0 +1,149 @@
+// This is the "final" code before refactoring for efficiency and readability. The end result looks identical to the refactored code, but is less performant and has some UI errors.
+// It's also really hard to read. I'm leaving it here for comparison purposes.
+
+let button = document.querySelector("#button");
+let inputs = document.querySelectorAll("input");
+let profitThreshold = document.querySelector(".profit-threshold");
+let audioNotification = document.querySelector("#audio-notification");
+
+class Calculator {
+  constructor() {
+    this.number = document.querySelector("#number");
+    this.deaths = document.querySelector("#deathCost");
+    this.chaos = document.querySelector("#chaosCost");
+    this.fires = document.querySelector("#fireCost");
+    this.tridents = document.querySelector("#tridentCost");
+    this.sellValue = document.querySelector("#sellPrice");
+    this.deathsCost = document.querySelector(".scaledDeaths");
+    this.chaosCost = document.querySelector(".scaledChaos");
+    this.fireCost = document.querySelector(".scaledFires");
+    this.tridentsCost = document.querySelector(".scaledTridents");
+    this.castsCost = document.querySelector(".scaledCasts");
+    this.scaledSales = document.querySelector(".scaledSales");
+    this.combinedCostElement = document.querySelector(".combinedCost");
+    this.taxCost = document.querySelector(".taxCost");
+    this.totalProfit = document.querySelector(".totalProfit");
+    this.tax = 0.01;
+    this.totalTax = 0;
+    this.combinedCost = 0;
+    this.saleValue;
+  }
+  async setValuesFromGE() {
+    let itemInfo = {};
+    await fetch("https://prices.runescape.wiki/api/v1/osrs/latest")
+      .then((res) => res.json())
+      .then((data) => {
+        itemInfo.deathRune = data.data[560];
+        itemInfo.chaosRune = data.data[562];
+        itemInfo.fireRune = data.data[554];
+        itemInfo.unchargedTrident = data.data[11908];
+        itemInfo.chargedTrident = data.data[11905];
+
+        this.deaths.value = itemInfo.deathRune.low;
+        this.fires.value = itemInfo.fireRune.low;
+        this.chaos.value = itemInfo.chaosRune.low;
+        this.tridents.value = itemInfo.unchargedTrident.low;
+        this.sellValue.value = itemInfo.chargedTrident.high;
+
+        this.renderAll();
+      });
+  }
+  renderTridentCost() {
+    const cost = this.number.value * this.tridents.value;
+    this.combinedCost = this.combinedCost + cost;
+    this.tridentsCost.innerHTML = `${cost.toLocaleString()} (${this.number.value.toLocaleString()})`;
+  }
+  renderDeathsCost() {
+    const cost = this.number.value * this.deaths.value * 2500;
+    this.combinedCost = this.combinedCost + cost;
+    this.deathsCost.innerHTML = `${cost.toLocaleString()} (${(
+      2500 * this.number.value
+    ).toLocaleString()})`;
+  }
+  renderChaosCost() {
+    const cost = this.number.value * this.chaos.value * 2500;
+    this.combinedCost = this.combinedCost + cost;
+    this.chaosCost.innerHTML = `${cost.toLocaleString()} (${(
+      2500 * this.number.value
+    ).toLocaleString()})`;
+  }
+  renderFiresCost() {
+    const cost = this.number.value * this.fires.value * 12500;
+    this.combinedCost = this.combinedCost + cost;
+    this.fireCost.innerHTML = `${cost.toLocaleString()} (${(
+      12500 * this.number.value
+    ).toLocaleString()})`;
+  }
+  renderCastsCost() {
+    const CAST_COST = 10;
+    const cost = this.number.value * CAST_COST * 2500;
+    this.combinedCost = this.combinedCost + cost;
+    this.castsCost.innerHTML = `${cost.toLocaleString()} (${(
+      2500 * this.number.value
+    ).toLocaleString()})`;
+  }
+  renderSaleValue() {
+    this.saleValue = this.sellValue.value * this.number.value;
+    this.scaledSales.innerHTML = this.saleValue.toLocaleString();
+  }
+  renderTax() {
+    this.totalTax = this.tax * this.saleValue;
+    this.taxCost.innerHTML = this.totalTax.toLocaleString();
+  }
+  renderCombinedCost() {
+    this.combinedCostElement.innerHTML = this.combinedCost.toLocaleString();
+  }
+  renderProfit() {
+    this.totalProfit.innerHTML = Math.round(
+      this.saleValue - this.combinedCost - this.tax
+    ).toLocaleString();
+  }
+
+  renderAll() {
+    this.combinedCost = 0;
+    this.renderTridentCost();
+    this.renderChaosCost();
+    this.renderDeathsCost();
+    this.renderFiresCost();
+    this.renderCastsCost();
+    this.renderSaleValue();
+    this.renderTax();
+    this.renderCombinedCost();
+    this.renderProfit();
+    if (document.querySelector("#notify").checked) {
+      const singleProfit =
+        Math.round(this.saleValue - this.combinedCost - this.tax) /
+        this.number.value;
+      if (singleProfit >= profitThreshold.value) {
+        audioNotification.play();
+      }
+    }
+  }
+}
+let calculator = new Calculator();
+calculator.setValuesFromGE();
+calculator.renderAll();
+inputs.forEach((input) =>
+  input.addEventListener("input", () => {
+    calculator = new Calculator();
+    calculator.renderAll();
+  })
+);
+button.addEventListener("click", () => window.location.reload());
+let autoupdateInterval;
+document.querySelector("#autoupdate").addEventListener("change", () => {
+  if (document.querySelector("#autoupdate").checked) {
+    autoupdateInterval = setInterval(() => {
+      calculator.setValuesFromGE();
+      calculator.renderAll();
+    }, 120000);
+  } else {
+    clearInterval(autoupdateInterval);
+  }
+});
+
+function resetAndRender() {
+  document.querySelector("form").reset();
+  calculator = new Calculator();
+  calculator.renderAll();
+}
